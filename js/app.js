be3b0f018f8e12e2d2142832c33c237a98e7da5a
Nate Jonah <nate@natejonah.com>
2017-12-22 00:13:20 +0000

    Remove jQuery and replace with plain JavaScript

--- js/app.js
+++ js/app.js
@@ -1,71 +1,97 @@
-// ===== Current location: ====== //
-if (navigator.geolocation) {
+function app() {
+  const container = document.getElementById("container"),
+        location = document.getElementById("location"),
+        icon = document.querySelector(".weather-icon"),
+        conditions = document.querySelector(".conditions"),
+        celsius = document.getElementById("cel"),
+        fahrenheit = document.getElementById("fah"),
+        tempButtons = document.querySelector(".temp-buttons"),
+        fahrenheitButton = document.querySelector(".f-button"),
+        celsiusButton = document.querySelector(".c-button"),
+        errorMessage = document.createElement("h3");
 
-  function success(position) {
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    getWeather(lat, lng);
+  // ===== Current location: ====== //
+  if (navigator.geolocation) {
+
+    function success(position) {
+      // When location is successfully retrieved
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      getWeather(lat, lng);
+    }
+
+    function error() {
+      // Create two elements to display error messages when location access is denied.
+      const subErrorMessage = document.createElement("h4");
+      errorMessage.textContent = "Unable to retrieve weather conditions.";
+      subErrorMessage.textContent = "Please allow location access.";
+      container.appendChild(errorMessage);
+      container.appendChild(subErrorMessage);
+      subErrorMessage.classList.add("error");
+
+      // Hide the elements that hold weather info
+      const weatherElements = [location, icon, conditions, celsius, fahrenheit, tempButtons];
+      weatherElements.forEach(element => {
+        element.classList.add("hide");
+      });
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else if (!navigator.geolocation) {
+    // If user's browser doesn't support geolocation
+    errorMessage.textContent = "Geolocation is not supported by your browser.";
+    container.appendChild(errorMessage);
   }
 
-  function error() {
-    $("<h3>").text("Unable to retrieve weather conditions.").appendTo(".container");
-    $("<h4>").text("Please allow location access.").appendTo(".container");
-    $(".container p, .conditions, #cel, #fah").hide();
+  // convert temp to fahrenheit
+  function getFahrenheit(celsius) {
+    return Math.round(celsius * 9 / 5 + 32);
   }
 
-  navigator.geolocation.getCurrentPosition(success, error);
-} else if (!navigator.geolocation) {
-  $("<h3>").text("Geolocation is not supported by your browser.").appendTo(".container");
-}
+  // ===== Get weather icon and add it to the DOM ===== //
+  function getIcon(id) {
+    const iconClass = "wi-owm-" + id;
+    document.getElementsByTagName("i")[0].classList.add(iconClass);
+  }
 
-// converter
-// to f
-function getFahrenheit(celsius) {
-  return Math.round(celsius * 9 / 5 + 32);
-}
-// to c
-function getCelsius(fahrenheit) {
-  return Math.round((fahrenheit - 32) * 5 / 9);
-}
+  // ===== Get current weather: ====== //
+  function getWeather(latitude, longitude) {
+    const weatherData = "//fcc-weather-api.glitch.me/api/current?lon=" + longitude + "&lat=" + latitude;
 
-//icons
-function getIcon(id) {
-  var iconClass = "wi-owm-" + id;
-  $("i").addClass(iconClass);
-}
+    const weather = new XMLHttpRequest();
+    weather.onreadystatechange = function() {
+      if (weather.readyState === 4) {
+        if (weather.status === 200) {
+          // turn weather data from API into an object
+          const weatherInfo = JSON.parse(weather.responseText);
+          const temp = document.querySelector(".temp");
 
-// ===== Get current weather: ====== //
-function getWeather(latitude, longitude) {
-  var weatherData = "//fcc-weather-api.glitch.me/api/current?lon=" + longitude + "&lat=" + latitude;
-  $.getJSON(weatherData, function(weather) {
-    $(".location").text(weather.name);
-    $(".temp").text(Math.round(weather.main.temp));
-    $(".unit").html("&deg;C");
-    $(".conditions").text(weather.weather[0].main);
+          // Add weather info to the DOM
+          location.textContent = weatherInfo.name;
+          temp.textContent = Math.round(weatherInfo.main.temp);
+          conditions.textContent = weatherInfo.weather[0].main;
+          getIcon(weatherInfo.weather[0].id);
 
-    var iconId = weather.weather[0].id;
-    getIcon(iconId);
+          // Convert temperature to fahrenheit
+          const fTemp = document.querySelector(".f-temp");
+          fTemp.textContent = getFahrenheit(weatherInfo.main.temp);
 
-    //convert to fahrenheit
-    $(".f-temp").text(getFahrenheit(weather.main.temp));
-    $(".f-unit").html("&deg;F");
+          // Display these when the data comes through from the server
+          document.querySelector(".unit").innerHTML = "&deg;C";
+          fahrenheitButton.classList.remove("hide");
+        } else {
+          alert(weather.status);
+        }
+      }
+    };
+    weather.open("GET", weatherData);
+    weather.send();
+  }
 
-    // switch between celsius and fahrenheit
-    $("button").click(function() {
-      $("#cel").toggle();
-      $(".f-button").toggle();
-      $("#fah").toggle();
-      $(".c-button").toggle();
-    });
-
+  // Celsius & fahrenheit toggle
+  tempButtons.addEventListener("click", function(e) {
+    const tempElements = [fahrenheit, celsius, fahrenheitButton, celsiusButton];
+    tempElements.forEach(el => el.classList.toggle("hide"));
   });
 }
 
-$(".to-c").click(function() {
-  var f = $(".temp").text();
-  $(".temp").text(getCelsius(f));
-  $(".unit").html("&deg;C");
-  $(this).removeClass("to-c");
-  $(this).addClass("to-f");
-  $(this).html("&deg;F");
-});
+app();
